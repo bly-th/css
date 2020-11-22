6623a0cd9a307d0514b3ce36dff8c9df97086967
Ryan Gittings <ryan@madebykingfisher.co.uk>
2020-11-22 19:35:19 +0000

    Fix tests

--- __tests__/utility.test.js
+++ __tests__/utility.test.js
@@ -4,22 +4,22 @@ const utility = require('../src/utility/index').run;
 const projectConfig = require('../src/helpers/project-config.js')();
 
 test('utility file is generated', async () => {
-  await utility(['reset']);
-
-  const outputPath = projectConfig.utilityOutputPath;
-  expect(fs.existsSync(`${outputPath}/auto-grid.css`)).toBe(true);
+  utility(['reset']).then((_) => {
+    const outputPath = projectConfig.utilityOutputPath;
+    expect(fs.existsSync(`${outputPath}/auto-grid.css`)).toBe(true);
+  });
 });
 
 test('chained utility first file is generated', async () => {
-  await utility(['auto-grid', 'reset']);
-
-  const outputPath = projectConfig.utilityOutputPath;
-  expect(fs.existsSync(`${outputPath}/auto-grid.css`)).toBe(true);
+  utility(['auto-grid', 'reset']).then((_) => {
+    const outputPath = projectConfig.utilityOutputPath;
+    expect(fs.existsSync(`${outputPath}/auto-grid.css`)).toBe(true);
+  });
 });
 
 test('chained utility second file is generated', async () => {
-  await utility(['auto-grid', 'reset']);
-
-  const outputPath = projectConfig.utilityOutputPath;
-  expect(fs.existsSync(`${outputPath}/reset.css`)).toBe(true);
+  utility(['auto-grid', 'reset']).then((_) => {
+    const outputPath = projectConfig.utilityOutputPath;
+    expect(fs.existsSync(`${outputPath}/reset.css`)).toBe(true);
+  });
 });
