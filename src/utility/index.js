4eab2f712856cb54def862784c5942e8b6390280
ryangittings <ryan@madebykingfisher.co.uk>
2021-08-17 08:48:14 +0100

    Fix promises utility error

--- src/utility/index.js
+++ src/utility/index.js
@@ -1,12 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const chalk = require('chalk');
 
 const projectConfig = require('../helpers/project-config.js')();
 
-const run = async (tokenNames) => {
+const run = (tokenNames) => {
   if (tokenNames.length) {
-    tokenNames.forEach(async (token) => {
+    tokenNames.forEach((token) => {
       const inputPath = path.join(__dirname, `css/${token}.css`);
       const outputPath = `${projectConfig.utilityOutputPath}/${token}.css`;
 
