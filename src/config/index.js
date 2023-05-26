fda042a453f4b2956dd7442ef5ee7dfc1c11f677
ryangittings <ryan@madebykingfisher.co.uk>
2023-05-26 14:05:57 +0100

    Fix tokens

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const run = async () => {
   const inputPath = path.join(__dirname, `../tokens/default/config.js`);
-  const outputPath = './blyth.config.js';
+  const outputPath = './tailwind.config.js';
 
   await fs.promises.copyFile(inputPath, outputPath);
 };
